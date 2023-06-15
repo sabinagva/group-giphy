213590d42629f56ee5b0004ea07ba30cdd2a48d2
Zackaria Antar <antarzack@gmail.com>
2023-06-15 16:09:24 -0500

    completed put on favorite item.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import SearchForm from '../SearchForm/SearchForm';
+import FavoriteItem from '../FavoriteItem/FavoriteItem';
 
 
 
 function App(props) {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
-      <Router>
-        <Route exact path='/'>
-          <SearchForm />
-        </Route>
-      </Router>
-    </div>
+		<div>
+			<h1>Giphy Search!</h1>
+			<FavoriteItem item={{id: 1, url:'http://cdn.pixabay.com/photo/2023/05/29/19/24/peonies-8027028_1280.jpg'}}/>
+			<Router>
+				<Route exact path="/">
+					<SearchForm />
+				</Route>
+			</Router>
+		</div>
   );
 }
 
