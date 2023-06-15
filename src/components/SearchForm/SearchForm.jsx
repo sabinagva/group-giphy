5d461382cf6c2defa5b18ae247dd315de3a08a91
Adam Oberle <oberle.adam@gmail.com>
2023-06-15 11:38:14 -0500

    work on SearchForm.jsx possibly complete

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -7,13 +7,20 @@ function SearchForm () {
 
     let [searchInput, setSearchInput] = useState('')
 
+    const searchGifs = (event) => {
+        event.preventDefault();
+        console.log('Search input is:', searchInput)
+        dispatch({ type: 'GET_GIF', payload: searchInput})
+        setSearchInput('')
+    } 
+
     return (
-        <form>
+        <form onSubmit={searchGifs}>
             <input 
             type='text'
             placeholder='Search for GIFs Here!'
             value={searchInput}
-            onChange={(event) => setSearchInput(event)}
+            onChange={(event) => setSearchInput(event.target.value)}
             />
             <input type='submit' value='SEARCH' />
         </form>
