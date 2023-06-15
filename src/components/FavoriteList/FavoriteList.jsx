61c194d79e3dd7d5d5ec670562d32105e087d085
Will Richardson <rich1051@umn.edu>
2023-06-15 15:17:29 -0500

    Routing performed in App, FavoriteList, and SearchForm

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import FavoriteItem from "../FavoriteItem/FavoriteItem";
 import { useDispatch, useSelector } from "react-redux";
+import { HashRouter as Router, Link } from "react-router-dom";
 
 function FavoriteList() {
   const dispatch = useDispatch();
@@ -12,11 +13,15 @@ function FavoriteList() {
   }, []);
 
   return (
+    <Router>
+    <h2>Welcome to Your Favorites Page!</h2>
+        <Link to="/">Go Back to Homepage</Link>
     <div>
       {favoriteList.map((item) => {
         return <FavoriteItem key={item.id} item={item} />;
       })}
     </div>
+    </Router>
   );
 }
 
