c6a18dc4f9a75a12495c52fb380f18d2b00ef633
Zackaria Antar <antarzack@gmail.com>
2023-06-15 16:11:42 -0500

    completed put on favorite item.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import SearchForm from '../SearchForm/SearchForm';
-import FavoriteItem from '../FavoriteItem/FavoriteItem';
 
 
 
@@ -9,7 +8,6 @@ function App(props) {
   return (
 		<div>
 			<h1>Giphy Search!</h1>
-			<FavoriteItem item={{id: 1, url:'http://cdn.pixabay.com/photo/2023/05/29/19/24/peonies-8027028_1280.jpg'}}/>
 			<Router>
 				<Route exact path="/">
 					<SearchForm />
