2528d57a9968a29f096397c650dc9f03da6dfe26
Zackaria Antar <antarzack@gmail.com>
2023-06-15 17:02:30 -0500

    fixed post and second get.

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -6,23 +6,27 @@ const SearchResult = () => {
 const gifListReducer = useSelector (store => store.gifList)
 
     const dispatch = useDispatch();
-    const addNewGif = (url, event) => {
-        event.preventDefault()
-        dispatch({type:'ADD_GIF', payload: url});
 
 
-    }
+    return (
+		<div>
+			{gifListReducer.map((gif, i) => (
+				<div key={i}>
+					<img src={gif.images.original.url}></img>
 
-    return(
-        <div> 
-            {gifListReducer.map ((gif,i) => (
-                <div key={i} >
-                <img src={gif.images.original.url}></img>
-                
-                <button onClick={() => dispatch({type:'ADD_GIF', payload: gif.images.original.url})}>Add Favorite</button>
-                </div>
-            ))}       
-        </div>
-    )
+					<button
+						onClick={() =>
+							dispatch({
+								type: "ADD_GIF",
+								payload: gif.images.original.url,
+							})
+						}
+					>
+						Add Favorite
+					</button>
+				</div>
+			))}
+		</div>
+	);
 }
 export default SearchResult
